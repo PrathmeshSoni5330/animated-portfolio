b1e589e82a60cc10433a2b6fb19fda8ca333c39b
Prathmesh Soni <prathmeshsoni.24@kgpian.iitkgp.ac.in>
2025-02-15 01:47:58 +0530

    go

--- utils/contact.js
+++ utils/contact.js
@@ -6,10 +6,10 @@ const contact = {
     description: `I’m open for new opportunities – especially ambitious or large projects. However, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!.`,
     button: {
         label: 'Say Hello',
-        onClick: () => openLink('mailto:afzalimdad9@gmail.com?subject=Hello')
+        onClick: () => openLink('mailto:p.prathmeshsoni5330@gmail.com?subject=Hello')
     },
-    designAndBuiltBy: 'Designed & Built By Afzal Imdad',
-    handleBuiltByClick: () => openLink('https://github.com/afzalimdad9')
+    designAndBuiltBy: 'Designed & Built By Prathmesh Soni',
+    handleBuiltByClick: () => openLink('www.linkedin.com/in/prathmesh-soni')
 }
 
 export default contact
